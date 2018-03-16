bb691c3a1447c362967795eb1bb16bc32278893c
Leigh Halliday <leighhalliday@gmail.com>
2018-03-16 19:36:54 -0400

    Loading nearby cities using geolocation + localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PlacesAutocomplete from "react-places-autocomplete";
 import { inject, observer } from "mobx-react";
+import axios from "axios";
+
 import {
   Container, // Wrapper (div) for all the content in this component
   Logo, // The logo image (img)
@@ -16,7 +18,35 @@ import logo from "../images/theclima.svg";
 
 export default class Home extends React.Component {
   state = {
-    inputValue: ""
+    inputValue: "",
+    nearby: []
+  };
+
+  componentDidMount() {
+    const nearby = localStorage.getItem("nearby");
+    if (nearby) {
+      this.setState({
+        nearby: JSON.parse(nearby)
+      });
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.loadNearby(position.coords);
+      });
+    }
+  }
+
+  loadNearby = async coords => {
+    const response = await axios.get(
+      "https://abnormal-weather-api.herokuapp.com/cities/nearby",
+      { params: { latitude: coords.latitude, longitude: coords.longitude } }
+    );
+
+    this.setState({
+      nearby: response.data
+    });
+    localStorage.setItem("nearby", JSON.stringify(response.data));
   };
 
   render() {
@@ -41,6 +71,16 @@ export default class Home extends React.Component {
             }}
           />
         </Form>
+
+        <NearbyCities>
+          {this.state.nearby.map(city => (
+            <NearbyCity key={city.id}>
+              <NearbyLink to={`/${city.name}, ${city.region}, ${city.country}`}>
+                {city.name}
+              </NearbyLink>
+            </NearbyCity>
+          ))}
+        </NearbyCities>
       </Container>
     );
   }
