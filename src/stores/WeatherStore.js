bea04f32d52cc54de5f66d2ad027aa4104837271
Leigh Halliday <leighhalliday@gmail.com>
2018-03-16 19:47:35 -0400

    Cleaning up initial store state

--- src/stores/WeatherStore.js
+++ src/stores/WeatherStore.js
@@ -2,17 +2,12 @@ import { observable, action } from "mobx";
 import axios from "axios";
 
 class WeatherStore {
-  @observable nearby = [];
-  @observable address = "";
   @observable time = new Date().toISOString();
   @observable weather = null;
   @observable loadWeatherError = false;
 
   @action tick = () => {};
-  @action loadNearby = async () => {};
-  @action loadLocalNearby = () => {};
   @action fetchWeather = async city => {};
-  getPosition = (options = {}) => {};
 }
 
 export default new WeatherStore();
